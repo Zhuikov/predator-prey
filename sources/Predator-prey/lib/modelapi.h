2327cc1964458fa5eefdcf2ae5e1cae50bac0772
Zhuikov <artem.zhuikov@mail.ru>
2016-03-19 22:08:14 +0300

    Еще сырое API

--- sources/Predator-prey/lib/modelapi.h
+++ sources/Predator-prey/lib/modelapi.h
@@ -1,16 +1,17 @@
 #ifndef MODELAPI_H
 #define MODELAPI_H
-
+#include "predator.h"
+#include "prey.h"
+#include <vector>
 
 class ModelAPI
 {
 public:
-    ModelAPI();
-    void initializeModel();
-    void saveModel();
-    void loadModel();
-    void changeTime();
-    int checkPredators();
-    int checkPreys();
+    virtual void initializeModel()=0;
+    virtual void saveModel()=0;
+    virtual void loadModel()=0;
+    virtual void changeTime()=0;
+    virtual int checkPredators(const std::vector< Predator > &predatorVector)=0;
+    virtual int checkPreys(const std::vector< Prey > &preyVector)=0;
 };
 #endif // MODELAPI_H
--- sources/Predator-prey/lib/settings.h
+++ sources/Predator-prey/lib/settings.h
@@ -7,6 +7,8 @@ class Settings
     int field_length;
     int field_height;
     int day_without_meal;
+    int num_of_predators;
+    int num_of_preys;
 };
 
 #endif // SETTINGS_H
