a3f4deb16ec6c414bcf8b20c1ac7bcc90a2bc694
Zhuikov <artem.zhuikov@mail.ru>
2016-04-15 11:15:15 +0400

    Fixed cppcheck

--- sources/Predator-prey/lib/units.cpp
+++ sources/Predator-prey/lib/units.cpp
@@ -9,11 +9,11 @@ Units::Units()
 
 Units::~Units()
 {
-    for (std::vector<Predator*>::const_iterator i = predators.begin(); i != predators.end(); i++) {
+    for (std::vector<Predator*>::const_iterator i = predators.begin(); i != predators.end(); ++i) {
        delete *i;
     }
 
-    for (std::vector<Prey*>::const_iterator i = preys.begin(); i != preys.end(); i++) {
+    for (std::vector<Prey*>::const_iterator i = preys.begin(); i != preys.end(); ++i) {
         delete *i;
     }
 
