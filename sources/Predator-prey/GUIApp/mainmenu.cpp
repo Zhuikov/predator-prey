a391d54573a6017b56c598d90ba3683d482690f5
Zhuikov <artem.zhuikov@mail.ru>
2016-05-09 19:59:42 +0300

    кнопка выхода

--- sources/Predator-prey/GUIApp/mainmenu.cpp
+++ sources/Predator-prey/GUIApp/mainmenu.cpp
@@ -2,10 +2,23 @@
 
 MainMenu::MainMenu(QWidget* parent)
 {
-    this->setFixedSize(SCREEN_SIZE);
+    this->setFixedSize(WINDOW_SIZE);
 
-    exit_button = new QPushButton(this);
-   // exit_button->setStyleSheet(button_style);
-    exit_button->setText("Выход");
+    exit_button = new QPushButton("Выход", this);
+    exit_button->setStyleSheet(button_style);
     exit_button->resize(BUTTON_SIZE);
+    exit_button->move(WINDOW_SIZE.width() / 2 - BUTTON_SIZE.width() / 2,
+                      WINDOW_SIZE.height() / 2 - BUTTON_SIZE.height() / 2);
+    connect(exit_button, SIGNAL(clicked()), SLOT(close_window()));
+}
+
+void MainMenu::close_window()
+{
+    this->close();
+//    QWidget wgt(this);
+
+//    QPushButton* yes = new QPushButton("Да", &wgt);
+//    yes->setStyleSheet(button_style);
+//    yes->resize(150, 30);
+//    wgt.show();
 }
--- sources/Predator-prey/GUIApp/mainmenu.h
+++ sources/Predator-prey/GUIApp/mainmenu.h
@@ -6,25 +6,33 @@ class MainMenu : public QWidget
 {
     Q_OBJECT
 
-public:
-    MainMenu(QWidget *parent);
-private:
+    const QSize WINDOW_SIZE { 700, 540 };
+    const QSize BUTTON_SIZE { 300, 30 };
 
-    const QSize SCREEN_SIZE { 800, 640 };
-    const QSize BUTTON_SIZE { 350, 30 };
-
-//    QString button_style =
-//            "QPushButton {"
-//                "border: "
-//                "background: "
-//                "background: "
-//                "padding: "
-//                "border-radius: "
-//                "color: "
-//                "font-size: "
-//            "}";
+    QString button_style =
+            "QPushButton {"
+                " border: 1px solid #324ab2;"
+                " background: qlineargradient(x1: 0, y1: 1, x2: 0, y2: 0,  stop: 0 #ffb366, stop: 1 #ff4f00);"
+                " border-radius: 9px;"
+                " color: #324ab2;"
+                " font-size: 20px;"
+                " font-weight: bold;"
+            "}"
+            "QPushButton:pressed {"
+                " background: qlineargradient(x1: 0, y1: 1, x2: 0, y2: 0,  stop: 0 #ff7433, stop: 1 #ffb366);"
+            "}"
+            "QPushButton:hover {"
+                " color: #33ff33;"
+                " border: 1.5px solid #324ab2;"
+            "}";
 
     QPushButton* exit_button;
+
+public:
+    MainMenu(QWidget *parent);
+
+public slots:
+    void close_window();
 };
 
 #endif // MAINMENU_H
