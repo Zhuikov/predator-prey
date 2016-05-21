1118f321511924f7d99643ab2bfa526efd2a23b3
Karina <k.vilegzhanina@gmail.com>
2016-05-21 21:37:38 +0300

    Установка jupyter

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+echo "Install C++ build tools"
 apt-get update
 apt-get install -y build-essential qt5-default
 
+echo "Install anaconda"
+wget --quiet https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+
+chmod +x Miniconda3-latest-Linux-x86_64.sh
+./Miniconda3-latest-Linux-x86_64.sh -b -p /opt/anaconda
+cat >> /home/vagrant/.bashrc << END
+  # For anaconda
+  PATH=/opt/anaconda/bin:$PATH
+END
+
+echo "Install jupiter"
+/opt/anaconda/bin/conda install jupyter
