676202572c91a3103fcd848cdbca9811b330c678
Karina Vilegzhanina <k.vilegzhanina@gmail.com>
2016-06-15 00:00:27 +0300

    Теперь будет запускать тетрадки в тревисе

---
+++ notebooks/nbconv.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/opt/anaconda/bin/jupyter nbconvert --to notebook --execute /notebooks/LifeModel.ipynb
+/opt/anaconda/bin/jupyter nbconvert --to notebook --execute /notebooks/ModelLaunchExample.ipynb
