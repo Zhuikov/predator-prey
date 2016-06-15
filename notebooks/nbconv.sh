318f50295fabed27d0c3a9183459056130a763bf
Natalia Bogach <n.v.bogach@gmail.com>
2016-06-15 20:59:17 +0300

    Отделен первый раздел

--- notebooks/nbconv.sh
+++ notebooks/nbconv.sh
@@ -3,4 +3,4 @@
 /opt/anaconda/bin/jupyter nbconvert --to notebook --execute /repo/notebooks/LifeModel.ipynb
 cp /repo/sources/Predator-prey/CLI/CLI /usr/bin/pp-model
 /opt/anaconda/bin/jupyter nbconvert  --ExecutePreprocessor.timeout=6000 --to notebook --execute /repo/notebooks/AgentsToSteps.ipynb
-/opt/anaconda/bin/jupyter nbconvert  --ExecutePreprocessor.timeout=6000 --to notebook --execute /repo/notebooks/SeedToSteps.ipynb
+/opt/anaconda/bin/jupyter nbconvert  --ExecutePreprocessor.timeout=6000 --to notebook --execute /repo/notebooks/SeedToSteps2.ipynb
