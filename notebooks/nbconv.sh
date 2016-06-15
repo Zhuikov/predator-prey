97f823c1c24187ec984214c3c628e85c060da6a1
Karina Vilegzhanina <k.vilegzhanina@gmail.com>
2016-06-15 22:42:32 +0400

    Update nbconv.sh

--- notebooks/nbconv.sh
+++ notebooks/nbconv.sh
@@ -3,4 +3,5 @@
 /opt/anaconda/bin/jupyter nbconvert --to notebook --execute /repo/notebooks/LifeModel.ipynb
 cp /repo/sources/Predator-prey/CLI/CLI /usr/bin/pp-model
 /opt/anaconda/bin/jupyter nbconvert  --ExecutePreprocessor.timeout=6000 --to notebook --execute /repo/notebooks/AgentsToSteps.ipynb
-/opt/anaconda/bin/jupyter nbconvert  --ExecutePreprocessor.timeout=6000 --to notebook --execute /repo/notebooks/SeedToSteps2.ipynb
+time pp-model S 200 200 3000 3000 13
+#/opt/anaconda/bin/jupyter nbconvert  --ExecutePreprocessor.timeout=6000 --to notebook --execute /repo/notebooks/SeedToSteps2.ipynb
