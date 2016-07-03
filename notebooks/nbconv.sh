bff9ea970623401d4218b41815523f84ebbfd284
lezhenin <yurij.lezhenin@yandex.ru>
2016-07-03 18:03:38 +0300

    Эксперименты запускються в тервисе

--- notebooks/nbconv.sh
+++ notebooks/nbconv.sh
@@ -4,5 +4,9 @@ time /opt/anaconda/bin/jupyter nbconvert --to html --execute LifeModel.ipynb
 time /opt/anaconda/bin/jupyter nbconvert --to html --execute DynamicLifeModel.ipynb
 
 cp /repo/sources/Predator-prey/CLI/CLI /usr/bin/pp-model
+time /opt/anaconda/bin/jupyter nbconvert  --ExecutePreprocessor.timeout=6000 --to html --execute Experiment_1.ipynb
+time /opt/anaconda/bin/jupyter nbconvert  --ExecutePreprocessor.timeout=6000 --to html --execute Experiment_2.ipynb
+time /opt/anaconda/bin/jupyter nbconvert  --ExecutePreprocessor.timeout=6000 --to html --execute Experiment_3.ipynb
+time /opt/anaconda/bin/jupyter nbconvert  --ExecutePreprocessor.timeout=6000 --to html --execute Experiment_4.ipynb
 time /opt/anaconda/bin/jupyter nbconvert  --ExecutePreprocessor.timeout=6000 --to html --execute Experiment_5.ipynb
-time /opt/anaconda/bin/jupyter nbconvert  --ExecutePreprocessor.timeout=6000 --to html --execute SeedToSteps.ipynb
+
