2d300854bdb4241cbfcbbbb5e237b874548d78c4
lezhenin <yurij.lezhenin@yandex.ru>
2016-07-03 18:16:16 +0300

    Путь к юпитеру добавлен в PATH

--- notebooks/nbconv.sh
+++ notebooks/nbconv.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+export PATH=$PATH:/opt/anaconda/bin/
 
-time /opt/anaconda/bin/jupyter nbconvert --to html --execute LifeModel.ipynb
-time /opt/anaconda/bin/jupyter nbconvert --to html --execute DynamicLifeModel.ipynb
+time jupyter nbconvert --to html --execute LifeModel.ipynb
+time jupyter nbconvert --to html --execute DynamicLifeModel.ipynb
 
 cp /repo/sources/Predator-prey/CLI/CLI /usr/bin/pp-model
-time /opt/anaconda/bin/jupyter nbconvert  --ExecutePreprocessor.timeout=6000 --to html --execute Experiment_1.ipynb
-time /opt/anaconda/bin/jupyter nbconvert  --ExecutePreprocessor.timeout=6000 --to html --execute Experiment_2.ipynb
-time /opt/anaconda/bin/jupyter nbconvert  --ExecutePreprocessor.timeout=6000 --to html --execute Experiment_3.ipynb
-time /opt/anaconda/bin/jupyter nbconvert  --ExecutePreprocessor.timeout=6000 --to html --execute Experiment_4.ipynb
-time /opt/anaconda/bin/jupyter nbconvert  --ExecutePreprocessor.timeout=6000 --to html --execute Experiment_5.ipynb
+time jupyter nbconvert  --ExecutePreprocessor.timeout=6000 --to html --execute Experiment_1.ipynb
+time jupyter nbconvert  --ExecutePreprocessor.timeout=6000 --to html --execute Experiment_2.ipynb
+time jupyter nbconvert  --ExecutePreprocessor.timeout=6000 --to html --execute Experiment_3.ipynb
+time jupyter nbconvert  --ExecutePreprocessor.timeout=6000 --to html --execute Experiment_4.ipynb
+time jupyter nbconvert  --ExecutePreprocessor.timeout=6000 --to html --execute Experiment_5.ipynb
 
