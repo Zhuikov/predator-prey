a9186edb1d8306d85c1a1acf563a0dccdd8b7d92
vilegzhanina <k.vilegzhanina@gmail.com>
2016-07-06 22:58:55 +0400

    Update nbconv.sh

--- notebooks/nbconv.sh
+++ notebooks/nbconv.sh
@@ -5,9 +5,9 @@ time jupyter nbconvert --to html --execute LifeModel.ipynb
 time jupyter nbconvert --to html --execute DynamicLifeModel.ipynb
 
 cp /repo/sources/Predator-prey/CLI/CLI /usr/bin/pp-model
-time jupyter nbconvert  --ExecutePreprocessor.timeout=6000 --to html --execute Experiment_1.ipynb
-time jupyter nbconvert  --ExecutePreprocessor.timeout=6000 --to html --execute Experiment_2.ipynb
-time jupyter nbconvert  --ExecutePreprocessor.timeout=6000 --to html --execute Experiment_3.ipynb
-time jupyter nbconvert  --ExecutePreprocessor.timeout=6000 --to html --execute Experiment_4.ipynb
-time jupyter nbconvert  --ExecutePreprocessor.timeout=6000 --to html --execute Experiment_5.ipynb
+time jupyter nbconvert  --ExecutePreprocessor.timeout=60000 --to html --execute Experiment_1.ipynb
+time jupyter nbconvert  --ExecutePreprocessor.timeout=60000 --to html --execute Experiment_2.ipynb
+time jupyter nbconvert  --ExecutePreprocessor.timeout=60000 --to html --execute Experiment_3.ipynb
+time jupyter nbconvert  --ExecutePreprocessor.timeout=60000 --to html --execute Experiment_4.ipynb
+time jupyter nbconvert  --ExecutePreprocessor.timeout=60000 --to html --execute Experiment_5.ipynb
 
