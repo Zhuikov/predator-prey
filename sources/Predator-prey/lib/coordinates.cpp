1f0e85aeb5fdbfc8a7edc00e6e78234a23f7dad2
Yurij Lezhenin <yurij.lezhenin@yandex.ru>
2016-11-07 21:17:10 +0300

    replace pow with *

--- sources/Predator-prey/lib/coordinates.cpp
+++ sources/Predator-prey/lib/coordinates.cpp
@@ -4,7 +4,8 @@
 double Coordinates::operator-(const Coordinates &a)
 {
     double distance_between_points = 0;
-    distance_between_points = sqrt(pow(vertical - a.getV(), 2) + pow(horizontal - a.getH(), 2));
+    distance_between_points = sqrt((vertical - a.getV()) * (vertical - a.getV()) +
+                                   (horizontal - a.getH()) * (horizontal - a.getH()));
 
     return distance_between_points;
 }
